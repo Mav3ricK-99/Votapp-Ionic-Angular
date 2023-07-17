e7cf4292a75096b2b222d0e7f748bebd2a9b7ab9
Federico Gutierrez <federico1999g@gmail.com>
2023-07-17 18:22:22 -0300

    Implementacion clase Usuario

--- src/app/classes/user/user.ts
+++ src/app/classes/user/user.ts
@@ -1,2 +1,39 @@
 export class User {
+    private id: number;
+    private name: string;
+    private surname: string;
+    private email: string;
+    private country: string;
+    private yearOfBirth: number;
+    private createdAt: Date = new Date();
+
+    constructor(id: number, name: string, surname: string, email: string, country: string, yearOfBirth: number) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.country = country;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public get getName(): string {
+        return this.name;
+    }
+
+    public get getEmail(): string {
+        return this.email;
+    }
+
+    public get getSurname(): string {
+        return this.surname;
+    }
+
+    public get getResidenceCountry(): string {
+        return this.country;
+    }
+
+    public get getYearOfBirth(): number {
+        return this.yearOfBirth;
+    }
+
 }
