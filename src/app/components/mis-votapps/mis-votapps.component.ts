5a712df64abb428ad1ab11c958bcdc9213fb4ba3
Federico Gutierrez <federico1999g@gmail.com>
2023-07-24 17:50:18 -0300

    Primera parte de 'mis votapps' :construction:

--- src/app/components/mis-votapps/mis-votapps.component.ts
+++ src/app/components/mis-votapps/mis-votapps.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-mis-votapps',
   templateUrl: './mis-votapps.component.html',
   styleUrls: ['./mis-votapps.component.scss'],
 })
-export class MisVotappsComponent  implements OnInit {
+export class MisVotappsComponent implements OnInit {
 
-  constructor() { }
+  public votapps$: Observable<any>;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {
+    this.votapps$ = this.userService.getMyVotes();
+  }
+
+  ngOnInit() { }
 
 }
