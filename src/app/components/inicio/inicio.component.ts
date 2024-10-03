e212aa751ad888129f807e0bc2d95fdf3dd3c52b
Federico Gutierrez <federico1999g@gmail.com>
2024-10-03 13:25:17 -0300

    Cambio el inicio de la app :sparkles: :construction:

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -5,13 +5,15 @@ import { MisVotappsComponent } from '../mis-votapps/mis-votapps.component';
 import { UserService } from 'src/app/services/user/user.service';
 import { Router, RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { animate, animation, state, style, transition, trigger } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonRow, IonCol, IonContent, IonTitle, IonButtons, IonMenuButton, UpperCasePipe, TranslateModule, MisVotappsComponent, RouterLink]
+  imports: [IonHeader, IonToolbar, IonRow, IonCol, IonContent, IonTitle, IonButtons, IonMenuButton, UpperCasePipe, TranslateModule, MisVotappsComponent, RouterLink],
 })
 export class InicioComponent implements OnInit {
 
@@ -33,37 +35,31 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.abrirLibro();
+      this.abrirPortada();
+      this.irMisVotapps();
     }, 1500);
   }
 
-  abrirLibro() {
-    if (!this.abriendoLibro) {
-      this.abriendoLibro = true;
-      this.bookWrap.nativeElement.style.left = '50px';
-
-      this.animacionLibro().then(() => {
-        setTimeout(() => {
-          this.router.navigateByUrl('dashboard/mis-votapps');
-        }, 3500);
-      })
+  toggleClass(e: any, toggleClassName: any) {
+    if (e.className.includes(toggleClassName)) {
+      e.className = e.className.replace(' ' + toggleClassName, '');
+    } else {
+      e.className += ' ' + toggleClassName;
     }
   }
 
-  animacionLibro() {
-    return new Promise((resolve: any) => {
-      setTimeout(() => {
-        this.tapaIzquierda.nativeElement.style.transform = 'translate3d(10%, 10px, 106px) rotateX(0deg) rotateY(-150deg) rotateZ(0deg)';
-        this.centroLibro.nativeElement.style.transform = 'translate3d(5px, 8px, -150px) rotateX(-4deg) rotateY(0deg) rotateZ(0deg)';
-        this.centroLibro.nativeElement.style.height = '114%';
-        this.primerPagina.nativeElement.style.transform = 'translate3d(12px, 5px, 75px) rotateX(0deg) rotateY(32deg) rotateZ(0deg)';
-        this.derechaLibro.nativeElement.style.transform = 'translate3d(0px, 0px, 0px)';
-        this.bookWrap.nativeElement.style.left = '0px';
-        this.tapaDerecha.nativeElement.style.transform = 'translate3d(0%, 11px, 59px)';
-        this.pageText.nativeElement.style.transform = 'translate3d(29vw, 0px, 10px)';
-      }, 2000);
-      resolve();
-    });
+  abrirPortada() {
+    let e: any = document.getElementById('primer');
+    this.toggleClass(e, "left-side");
+    this.toggleClass(e.nextElementSibling, "left-side");
   }
 
+  irMisVotapps() {
+    if (!this.abriendoLibro) {
+      this.abriendoLibro = true;
+      setTimeout(() => {
+        this.router.navigateByUrl('dashboard/mis-votapps');
+      }, 700);
+    }
+  }
 }
